5b892edab7febb733d7b01624c1559f382976f62
salmandotexe <salman.exe@gmail.com>
2018-05-17 18:55:41 +0600

    Update AramicScript.cpp

--- AramicScript.cpp
+++ AramicScript.cpp
@@ -1,34 +1,63 @@
 #include<iostream>
-#include<set>
-#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-string removeduplicates(string s)
+int findindex(int i)
 {
-  string ss="";
-  int arr[27]={0};
-  for(int i=0;i<s.size();i++)
+  int x;
+  x=((i+1)%14)-1;
+  if(x==-1)
+    return 13;
+  return x;
+}
+
+int computenew(int a, int *A)
+{
+  int w[14];
+  for(int i=0;i<14;i++)
   {
-    arr[s[i]-'a']++;
+    w[i]=A[i];
   }
-  for(int i=0;i<27;i++)
+  int temp=w[a],idx=a;//number of stones in hole i.
+  w[a]-=temp;
+
+
+  for(int i=0;i<14;i++)
   {
-    if(arr[i]>0)
-      ss+=(char)('a'+i);
+    w[i]+=temp/14;
   }
-  return ss;
+  temp%=14;
+
+  for(int i=1;temp!=0;i++)
+  {
+    int x=findindex(idx+i);
+    w[x]++;
+    temp--;
+  }
+  int res=0;
+  for(int i=0;i<14;i++)
+  {
+    res+=w[i]&1?0:w[i];
+  }
+  return res;
 }
 
 int main()
 {
-  set<string> roots;
-  string s;int N;
-  cin >> N;
-  while (N--)
+  int v[14];
+  for(int i=0;i<14;i++)
   {
-    cin >> s;
-    roots.insert(removeduplicates(s));
+    cin >> v[i];
   }
-  cout << roots.size()<<endl;
+  int m=-1;
+  for(int i=0;i<14;i++)
+  {
+    if(v[i]>0)
+    {
+      m=max(m,computenew(i,v));
+    }
+  }
+  cout << m;
 }
