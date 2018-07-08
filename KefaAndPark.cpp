dd17608dc801dfcfdffcd0dec5c936307d2dfff8
Salman Sahel <salman.exe@gmail.com>
2018-07-09 01:10:39 +0600

    Update KefaAndPark.cpp

--- KefaAndPark.cpp
+++ KefaAndPark.cpp
@@ -1,87 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-# define INF 0x3f3f3f3f
 
-//http://codeforces.com/problemset/problem/580/C
-//Verdict: Wrong Answer on Test 3.
 
-class Graph
-{
-    int V;
-    list<int> *adj;
+class Graph{
+  int V,E;
+  list<int> *ptr;
 public:
-    Graph(int V);
-    void addEdge(int u, int v);
-    void shortestPath(int s);
-    void DFS(int s, int *number, int *cats, int *res, int maxcats);
+  Graph(int v)
+  {
+    V=v;
+    ptr= new list<int>[V+1];
+  }
+  void addedge(int u, int v)
+  {
+    ptr[u].push_back(v);
+  }
+  void DFS(int s, int *cats, int *dist);
 };
 
-Graph::Graph(int V)
+void Graph::DFS(int s, int *cats, int *dist)
 {
-    this->V = V;
-    adj = new list<int>[V];
-}
-// prints all not yet visited vertices reachable from s
-void Graph::DFS(int s, int *number, int *cats, int *res, int maxcats)
-{
-    int visited[V]={0};
-    stack<int> stack;
-    stack.push(s);
-    int puss=0;
-    int f=0;
+  int visited[V+1]={0};
+  stack<int> stack;
+  stack.push(s);
 
-    if(cats[s])
-      f=1;
-
-    while (!stack.empty())
+  while (!stack.empty())
+  {
+    s = stack.top();
+    stack.pop();
+    if (!visited[s])
     {
-        s = stack.top();
-        stack.pop();
-        if(cats[s])
-          puss++;
-
-        if (!visited[s])
-        {
-            //cout << s << " ";
-            visited[s] = 1;
-            number[s]=puss+f; //f is offset for src containing cat.
-        }
-
-        if(adj[s].empty())  //leaf
-        {
-          if(number[s]>maxcats)
-            (*res)++;
-          puss=0;
-        }
-
-        for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
-            if (!visited[*i])
-                stack.push(*i);
+      visited[s] = 1;
+      //if(cats[s])
+        //dist[s]=1;
     }
+
+    for (auto i = ptr[s].begin(); i != ptr[s].end(); ++i)
+      if (!visited[*i])
+      {
+        stack.push(*i);
+        cats[*i]=cats[s]+cats[*i];
+
+      }
+
+      if(ptr[s].empty())
+      cout << "Leaf at "<<s << " has met "<< cats[s] << " cats so far\n";
+  }
 }
 
-void Graph::addEdge(int u, int v)
-{
-    adj[u].push_back(v);
-}
 
 int main()
 {
-    int V, maxcats,u,v,edges;
-    cin >> V >> maxcats;
-    Graph g(V);
-    int cat[V]={0};
-    for(int i=0; i<V;i++)
-      cin >> cat[i];
+  int n,k,maxcats,x;
+  cin >> n >> maxcats;
+  int cats[n+1];
+  int dist[n+1]={0};
 
-    edges=V-1;
-    while(edges--)
-    {
-      cin >> u >> v;
-      g.addEdge(u-1,v-1);
-    }
-    int number[V]={0};
-    int res=0;
-    g.DFS(0,number,cat,&res, maxcats);
-    cout << res << endl;
+  Graph g(n);
+  cats[0]=0;
+
+  for(int i=1;i<=n;i++)
+  {
+    cin >> x;
+    cats[i]=x;
+  }
+
+  k=n-1;
+  int u,v;
+
+  while(k--)
+  {
+    cin >> u >> v;
+    g.addedge(u,v);
+  }
+  g.DFS(1,cats,dist);
 }
