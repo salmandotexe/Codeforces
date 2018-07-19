877a2ac3c35e1715e0dffb8370366d57b5bae043
Salman Sahel <salman.exe@gmail.com>
2018-07-19 22:58:30 +0600

    Codeforces 597A: Divisibility

---
+++ Divisibility.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  long long a,b,k;
+  cin >> k >> a >> b;
+  if(a<=0 && b >=0)
+  {
+    a*=-1;
+    //b*=-1;
+    //swap(a,b);
+    cout << 1+abs(a/k+b/k);
+    cout << endl;
+    return 0;
+  }
+  else if(a<=0 && b<=0)
+  {
+    a*=-1;
+    b*=-1;
+    swap(a,b);
+  }
+  cout << b/k-(a-1)/k;
+  cout << endl;
+}
