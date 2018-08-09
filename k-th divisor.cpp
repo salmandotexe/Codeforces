62623fb677515d71c67df46b541adf012a594549
Salman Sahel <salman.exe@gmail.com>
2018-08-09 20:20:48 +0600

    Codeforces 762A: k-th divisor

---
+++ k-th divisor.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+  //vector<bool> notprime(1e8,false);
+  //notprime[0]=notprime[1]=true;
+
+
+  ll N, k;
+  cin >> N >> k;
+  ll sqa=sqrt(N);
+
+  set<ll> f;
+  f.insert(1);
+
+  for(ll i=2; i<=sqa;i++)
+  {
+    if(N%i==0)
+    {
+      f.insert(i);
+      f.insert(N/i);
+    }
+  }
+  f.insert(N);
+  auto a=f.begin();
+  if(k>f.size())
+  {
+    cout << -1<<endl;
+    return 0;
+  }
+  for(int i=0;i<k-1;i++)
+    a++;
+  cout << *a<<endl;
+}
