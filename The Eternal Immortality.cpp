5d16b7239681ecd007955b11840f595c6e5b874c
Salman Sahel <salman.exe@gmail.com>
2018-08-20 21:19:11 +0600

    Codefoces 869B: The Eternal Immortality

---
+++ The Eternal Immortality.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+
+int main()
+{
+  long long n,m;
+  cin >> n >> m;
+  if(m-n>=5)
+    cout << 0;
+  else
+  {
+    ll res=1;
+    for(ll i=n+1;i<=m;i++)
+      res*=i%10;
+    res%=10;
+    cout << res<<endl;
+  }
+}
