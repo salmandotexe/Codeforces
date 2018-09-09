b7fe23af73bb85c3e5b2ced836b559ea6cd85f05
Salman Sahel <salman.exe@gmail.com>
2018-09-09 21:44:14 +0600

    Create 1029C.cpp

---
+++ 1029C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+//WA on Test 7.
+using namespace std;
+typedef long long ll;
+
+pair<ll,ll> intersect(multiset<pair<ll,pair<ll,ll>> > &s)
+{
+  ll r=INT_MAX;
+  ll l=-1;
+  for(auto a:s)
+  {
+    l=max(a.second.first,l);
+    r=min(a.second.second,r);
+  }
+  pair<ll,ll> p=make_pair(l,r);
+  return p;
+}
+
+int main()
+{
+  int n;
+  ll a,b;
+  cin >> n;
+  multiset<pair<ll,pair<ll,ll>> > s;
+  while(n--)
+  {
+    cin >> a >> b;
+    s.insert(make_pair(b-a, make_pair(a,b)));
+  }
+  s.erase(s.begin());
+  pair<ll,ll> p =intersect(s);
+  if(p.first<=p.second)
+    cout << p.second-p.first<<endl;
+  else
+    cout << 0<<endl;
+}
