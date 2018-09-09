232a7143f314a49a662cf11fa8b9c862efc6158a
Salman Sahel <salman.exe@gmail.com>
2018-09-09 21:47:31 +0600

    Create 1029A.cpp

---
+++ 1029A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n,k;
+  string t;
+  cin >> n >> k >>t;
+  string s="";
+  if(t[0]==t[n-1] && t.size()!=1)
+    for(int i=0;i<n-1;i++)
+      s+=t[i];
+  else
+    s=t;
+  for(int i=0;i<k;i++)
+    cout << s;
+  if(t[0]==t[n-1]&& t.size()!=1)
+    cout << t[0];
+  cout << endl;
+}
