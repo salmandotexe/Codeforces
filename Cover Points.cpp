5a16259858743dab8574d6035cf5e06e56ae6be0
Salman Sahel <salman.exe@gmail.com>
2018-09-21 23:11:16 +0600

    Codeforces 1047B: Cover Points

---
+++ Cover Points.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+ll gcd(ll  a, ll b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b%a,a);
+}
+int main()
+{
+  int n;
+  cin >> n;
+  ll res=INT_MIN;
+  ll a,b;
+
+  ll x,y;
+  while(n--)
+  {
+    cin >> a >> b;
+    ll r=a*a + b*b;
+    if(r>res)
+    {
+      res=r;
+      x=a;y=b;
+    }
+  }
+  cout << ll(x+y)<<endl;
+
+}
