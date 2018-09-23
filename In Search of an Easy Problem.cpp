f7e32694d49350180c2119d47415429cd8b57a4e
Salman Sahel <salman.exe@gmail.com>
2018-09-23 21:46:38 +0600

    Codeforces 1048A: In Search of an Easy Problem

---
+++ In Search of an Easy Problem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+int main()
+{
+  int n,x;
+  cin >> n;
+  int res=0;
+  for(int i=0;i<n;i++)
+  {
+    cin >> x;
+    res=res||x;
+  }
+  if(res==1)
+    cout << "HARD\n";
+  else
+    cout << "EASY\n";
+}
