03768795da316a9e06a8adf2bc0a78281473bd09
Salman Sahel <salman.exe@gmail.com>
2018-11-20 23:59:11 +0600

    Update Good Array.cpp

--- Good Array.cpp
+++ Good Array.cpp
@@ -1,12 +1,41 @@
-//from Editorial
-/*
-  Calculate sum of whole array: sum.
-  maintain array cnt[i]= number of elements in given array that equals i.
-  
-  for(int i=0;i<cnt.size();i++) 
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define fi first
+#define se second
+#define pb push_back
+#define pbb pop_back
+#define mp make_pair
+#define pii pair<int,int>
+#define all(x) x.begin(), x.end()
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  vi v(n),cnt(int(1e6)+1),ans;
+  for(int i=0;i<n;i++)
   {
-    sum[i]=sum-arr[i];
-    cnt[arr[i]]--;
-    if sum is even && cnt[sum/2] >0 i is nice
+    cin >> v[i];
+    cnt[v[i]]++;
   }
-*/
+
+  ll sum=accumulate(all(v), 0ll);
+  for(int i=0;i<n;i++)
+  {
+    sum-=v[i];
+    --cnt[v[i]];
+    if(!(sum&1) && sum/2<=(int(1e6)) && cnt[sum/2]>0)
+      ans.pb(i+1);
+    sum+=v[i];
+    ++cnt[v[i]];
+  }
+  cout << ans.size()<<endl;
+  for(auto a: ans) cout << a<<" ";
+  cout <<endl;
+
+}
