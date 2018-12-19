5521c532e2c2491fedc1ea389d2eab47020b5062
Salman Sahel <salman.exe@gmail.com>
2018-12-20 01:26:50 +0600

    Codeforces 1092A: Univorm Strings

---
+++ Uniform Strings.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define vi vector<long long>
+#define ll long long
+
+using namespace std;
+ostream& operator << (ostream &out, vector<long long> &v)
+{
+  for(auto a:v)
+    out << a<<" ";
+  out<<endl;
+}
+
+int main()
+{
+  int t,n,k;
+  cin >> t;
+  while(t--)
+  {
+    cin >> n >> k;
+    string s="";
+    for(int i=0;i<n;i++)
+      s+=(char)('a'+i%k);
+    cout << s <<endl;
+  }
+}
