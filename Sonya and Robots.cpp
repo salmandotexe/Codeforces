2d04b5cdd94a1a850fea9b461614c3ec426ef723
Salman Sahel <salman.exe@gmail.com>
2019-01-02 14:21:11 +0600

    Codeforces 1004C: Sonya and Robots

---
+++ Sonya and Robots.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define ll long long
+using namespace std;
+
+int visited[int(1e5)+2];
+int dp[int(1e5)+2];
+ll res=0;
+int main()
+{
+  int n;
+  cin >> n;
+  vi v(n);
+  for(int i=0;i<n;i++)
+    cin >> v[i];
+
+  set<int> s;
+  for(int i=n-1;i>=0;i--)
+  {
+    dp[i]=s.size();
+    s.insert(v[i]);
+  }
+
+  for(int i=0;i<n;i++)
+  {
+    if(!visited[v[i]])
+    {
+      visited[v[i]]=1;
+      res+=dp[i];
+    }
+  }
+  cout << res<<endl;
+
+}
