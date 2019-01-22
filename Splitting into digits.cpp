712364956a7f13fdfdc0651cfbc1b0060a65461b
Salman Sahel <salman.exe@gmail.com>
2019-01-22 23:05:45 +0600

    Codeforces 1104A: Splitting into digits

---
+++ Splitting into digits.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+using namespace std;
+ostream& operator << (ostream &out, vector<int> &v)
+{
+  for(auto a:v)
+  out << a<< " ";
+  out<<endl;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  cout << n <<endl;
+  for(int i=0;i<n;i++) cout << 1 << " ";
+  cout<<endl;
+
+}
