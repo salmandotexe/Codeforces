32a8f1cee3a1dab4a4d1764efb8c14f0309c71bd
Salman Sahel <salman.exe@gmail.com>
2019-02-01 23:20:08 +0600

    Codeforces 1106A: Lunar New Year and Cross Counting

---
+++ Lunar New Year and Cross Counting.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+using namespace std;
+int main()
+{
+	int n;
+  cin >> n;
+  char arr[n+2][n+2];
+
+  for(int i=1;i<=n;i++)
+  for(int j=1;j<=n;j++) cin >> arr[i][j];
+  int cnt=0;
+  for(int i=1;i<=n;i++)
+  for(int j=1;j<=n;j++)
+  {
+    char c=arr[i][j];
+    if(c=='X' && c==arr[i-1][j-1] && c==arr[i+1][j+1] && c==arr[i+1][j-1] && c==arr[i-1][j+1])
+      cnt++;
+  }
+  cout <<cnt<<endl;
+
+}
