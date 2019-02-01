82308a4e9bb7b465aeccd6f1721804981b4256b9
Salman Sahel <salman.exe@gmail.com>
2019-02-01 23:20:52 +0600

    Codeforces 1106C: Lunar New Year and Number Division

---
+++ Lunar New Year and Number Division.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+using namespace std;
+int main()
+{
+	int n;
+  cin >> n;
+  vi v(n);
+  for(int i=0;i<n;i++) cin >> v[i];
+  sort(all(v));
+  ll x1=0,x2=0;
+  for(int i=0;i<n/2;i++)
+    x1+=(ll)(v[i]+v[n-i-1])*(v[i]+v[n-i-1]);
+  cout <<x1<<endl;
+}
