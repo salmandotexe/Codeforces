e0ff5a8b3fac1a162e5d9e379fa6f75dceb457b8
Salman Sahel <salman.exe@gmail.com>
2019-03-01 21:44:02 +0600

    Create F.cpp

---
+++ Gym 101291/F.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define pb push_back
+#define f first
+#define s second
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define precise fixed(cout);cout<<setprecision(16);
+using namespace std;
+
+ostream& operator << (ostream &out, vector<int> &v) { for(auto a:v) out << a<<" "; out<<endl; }
+
+int main()
+{
+  int a,b,c;
+  scanf("%d + %d = %d", &a,&b,&c);
+  cout << (c==a+b?"YES\n":"NO\n");
+}
