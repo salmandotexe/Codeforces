fe731768f5c2c40e4f962a1a3cd51af360413bd0
Salman Sahel <salman.exe@gmail.com>
2019-04-04 23:44:02 +0600

    Create A.cpp

---
+++ Gym 102055/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define pb push_back
+#define f first
+#define s second
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define precise fixed(cout);cout<<setprecision(16);
+using namespace std;
+
+ostream& operator << (ostream &out, vector<int> &v) { for(auto a:v) out << a<<" "; out<<endl; }
+
+
+int main()
+{
+  int T;
+  cin >> T;
+  for(int tc=1;tc<=T;tc++)
+  {
+    int n,m;
+    cin >> n >> m;
+    vector<pll> v(n);
+    for(int i=0;i<n;i++) cin >> v[i] .first;  //v[i].first is Di.
+    for(int i=0;i<n;i++) cin >> v[i].second;  //v[i].s is Ti.
+    sort(all(v));
+    //for(auto a:v) cout << a.f <<" " <<a.s <<endl;
+    ll res=0;
+    int cnt=0;
+    int idx=0;
+    while(res+v[idx].s<=m){
+      res+=v[idx++].second;
+      cnt++;
+    }
+    cout << "Case "<<tc<<": "<<cnt<<endl;
+  }
+}
