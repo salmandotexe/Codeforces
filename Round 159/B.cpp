ff5e42fd6a9b695b4d06af52d6a720bf672dcecb
Salman Sahel <salman.exe@gmail.com>
2019-06-07 23:58:11 +0600

    Update B.cpp

--- Round 159/B.cpp
+++ Round 159/B.cpp
@@ -1,1 +1,31 @@
-//
+#include <bits/stdc++.h>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define pb push_back
+#define f first
+#define s second
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define precise fixed(cout);cout<<setprecision(16);
+using namespace std;
+
+ostream& operator << (ostream &out, vector<int> &v) { for(auto a:v) out << a<<" "; out<<endl; }
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    int tot =n+m;
+    if(n<m) n=m;
+
+    cout << n-1 <<" " << tot-n << endl;
+}
