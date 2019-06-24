72c05674cbd6f0134b6dd80039fb1f7104cb8bec
Salman Sahel <salman.exe@gmail.com>
2019-06-24 22:55:27 +0600

    Create B.cpp

---
+++ Gyms/102219/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+long long a,b;
+cin >> a >> b;
+if(a==b) cout <<"YES\n";
+else cout <<"NO\n";
+}
+}
