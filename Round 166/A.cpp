a8cba8e8c8bb21488057a2002103b7b272dad282
Salman Sahel <salman.exe@gmail.com>
2019-07-23 14:03:11 +0600

    Create A.cpp

---
+++ Round 166/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define pb push_back
+#define f first
+#define s second
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define precise fixed(cout);cout<<setprecision(16);
+using namespace std;
+
+ostream& operator << (ostream &out, vector<int> &v) { for(auto a:v) out << a<<" "; out<<endl; }
+
+bool check(int i)
+{
+    vector<bool> dig(12);
+    while(i)
+    {
+        int rem=i%10;
+        if(dig[rem]) return 0;
+        else dig[rem]=1;
+        i/=10;
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int i=n+1;
+    for(;;i++) if(check(i)) break;
+    cout << i << endl;
+}
