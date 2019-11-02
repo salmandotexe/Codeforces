dd9a28f44391e266cb5f4e240dfc2fc33a0ef37f
Salman Sahel <salman.exe@gmail.com>
2019-11-02 22:20:31 +0600

    Update F.cpp

--- Gyms/2019-2020 ICPC, NERC, Southern and Volga Russian Regional Contest/F.cpp
+++ Gyms/2019-2020 ICPC, NERC, Southern and Volga Russian Regional Contest/F.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+const long long INF = 1e16;
+
+int main ()
+{
+    long long area;
+    cin >> area;
+    long long res = INF;
+    for (long long i = 1; i <= area; i++){
+        long long a, b;
+        if (area % i == 0){
+            a = area / i;
+            b = i;
+            res = min(res, 2 * a + 2 * b);
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
