b51ebb92d1abd693f4d50314fcce8ff96ed44157
Salman Sahel <salman.exe@gmail.com>
2019-11-05 21:59:41 +0600

    Update I.cpp

--- Gyms/101911/I.cpp
+++ Gyms/101911/I.cpp
@@ -1,1 +1,22 @@
 
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> v(n);
+    for(int i=0;i<n;i++) cin >> v[i];
+    sort(v.begin(),v.end());
+    ll res=0;
+    for(int i=0;i+1<n;i++)
+    {
+        if(v[i+1]!=v[i]+1)
+        {
+            res+=((v[i+1]-v[i])-1);
+        }
+    }
+    cout << res << endl;
+}
